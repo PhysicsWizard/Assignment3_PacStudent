24c7bbb7671349517a1e85776f295d2f93f2e7f9
Arie Maljers <ariemaljers@gmail.com>
2023-10-04 11:22:49 +1100

    Attempt to Check neighbours
    attempted to check neighbours

--- Assets/LevelGenerator.cs
+++ Assets/LevelGenerator.cs
@@ -1,3 +1,4 @@
+using System;
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
@@ -42,15 +43,72 @@ public class LevelGenerator : MonoBehaviour
                 if (levelMap[i,j] != 0)
                 {
                     Instantiate(tiles[levelMap[i,j]], new Vector2(-10 + j,3 - i), Quaternion.identity);
-                    Debug.Log("Piece " + tiles[levelMap[i,j]]);
+                    getRotation(levelMap[i,j],new Vector2(-10 + j,3 - i),levelMap);
                 }
             }
         }
     }
 
-    // Update is called once per frame
-    void Update()
+    private void getRotation(int tileKey, Vector2 position, int[,] array)
     {
+        Dictionary<char, GameObject> neighbours = getAdjacent(position,array);
+    }
+
+    private Dictionary<char,GameObject> getAdjacent(Vector2 position, int[,] array)
+    {
+        Dictionary<char, GameObject> cardinals = new Dictionary<char, GameObject>();
+
+        Vector2 north = position - new Vector2(0, 1) ;
+        Vector2 east = position + new Vector2(1,0);
+        Vector2 south = position + new Vector2(0, 1);
+        Vector2 west = position - new Vector2(1, 0);
         
+        cardinals['N'] = null;
+        
+        cardinals['E'] = null;
+        
+        cardinals['S'] = null;
+        
+        cardinals['W'] = null;
+
+        if (isInBounds(array,north))
+        {
+            cardinals['N'] = tiles[array[(int)north.x,(int)north.y]];
+        }
+
+        if (isInBounds(array,east))
+        {
+            cardinals['E'] = tiles[array[(int)east.x,(int)east.y]];
+        }
+        
+        if (isInBounds(array,south))
+        {
+            cardinals['S'] = tiles[array[(int)south.x,(int)south.y]];
+        }
+        
+        if (isInBounds(array,west))
+        {
+            cardinals['W'] = tiles[array[(int)west.x,(int)west.y]];
+        }
+        
+        foreach (KeyValuePair<char,GameObject> piece in cardinals)
+        {
+            char key = piece.Key;
+            if (piece.Value != null)
+            {
+                String name = piece.Value.name;
+                Debug.Log(key +": " + name);
+            }
+            else
+            {
+                Debug.Log(key +": " + "Empty"); 
+            }
+        }
+        return cardinals;
+    }
+
+    private bool isInBounds(int[,] array, Vector2 newPos)
+    {
+        return newPos.x >= 0 && newPos.x < array.GetLength(0) && newPos.y >= 0 && newPos.y < array.GetLength(1);
     }
 }
