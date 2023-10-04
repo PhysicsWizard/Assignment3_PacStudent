6d184117f09565aa07ba7f89eb5ed83ea9e4d1d7
Arie Maljers <ariemaljers@gmail.com>
2023-10-04 14:39:14 +1100

    Started Rotation mechanics
    Checks for neighbours works, completed rotation rules for outer corner.

--- Assets/LevelGenerator.cs
+++ Assets/LevelGenerator.cs
@@ -1,7 +1,11 @@
 using System;
 using System.Collections;
 using System.Collections.Generic;
+using System.Data;
+using System.Diagnostics;
+using Unity.Mathematics;
 using UnityEngine;
+using Debug = UnityEngine.Debug;
 
 public class LevelGenerator : MonoBehaviour
 {
@@ -26,8 +30,10 @@ public class LevelGenerator : MonoBehaviour
 
     public GameObject outWall, outCorner, inWall, inCorner, tJunk, pellet, pPellet;
     private Dictionary<int, GameObject> tiles = new Dictionary<int, GameObject>();
+    public GameObject grid;
     void Start()
     {
+        Destroy(grid);
         tiles[0] = null;
         tiles[1] = outCorner;
         tiles[2] = outWall;
@@ -36,56 +42,106 @@ public class LevelGenerator : MonoBehaviour
         tiles[5] = pellet;
         tiles[6] = pPellet;
         tiles[7] = tJunk;
+
+        Debug.Log(levelMap.GetLength(0));
+        Debug.Log(levelMap.GetLength(1));
+
         
-        for (int i = 0; i < levelMap.GetLength(0); i++)
+        for (int cols = 0; cols < levelMap.GetLength(0); cols++)
         {
-            for (int j = 0; j < levelMap.GetLength(1); j++)
+            for (int rows = 0; rows < levelMap.GetLength(1); rows++)
             {
-                if (levelMap[i,j] != 0)
+                if (tiles[levelMap[cols,rows]] != null)
                 {
-                
-                    Instantiate(tiles[levelMap[i,j]], new Vector2(-10 + j,3 - i), Quaternion.identity);
+                    Instantiate(tiles[levelMap[cols, rows]], new Vector2(rows,-cols),getRotation(levelMap[cols,rows], levelMap,cols,rows));
                 }
             }
-            
         }
-        foreach (KeyValuePair<char,GameObject> tile in getNeighbours(levelMap,7,3))
-        {
-            char key = tile.Key;
-            if (tile.Value != null)
-            {
-                String name = tile.Value.name;
-                Debug.Log(key + ": " + name);
-            }
-            else
-            {
-                Debug.Log(key + ": Empty.");
-            }
-        }
+        Debug.Log("NorthTest: " + tiles[getNorth(levelMap,4,1)].name);
+        Debug.Log("EastTest: " + tiles[getEast(levelMap,4,1)].name);
+        Debug.Log("NorthTest: " + tiles[getSouth(levelMap,4,1)].name);
+        Debug.Log("NorthTest: " + tiles[getWest(levelMap,4,1)].name);
+       
     }
 
-    private Dictionary<char,GameObject> getNeighbours(int[,] array, int xPos, int yPos)
+    private Quaternion getRotation(int tile, int[,] array, int x, int y)
     {
-        Dictionary<char, GameObject> piece = new Dictionary<char, GameObject>();
+        Quaternion rotationAngle = Quaternion.Euler(0,0,0);
+        
+        switch (tile)
+        {
+            case 1:
+                if (getEast(array,x,y) == 2 && getNorth(array,x,y) == 2)
+                {
+                    rotationAngle = Quaternion.Euler(0,0,90);
+                    Debug.Log("90");
+                }
 
-        if (isInBounds(array, xPos, yPos + 1))
-        {
-            piece['N'] = tiles[levelMap[xPos, yPos + 1]];
+                if (getWest(array,x,y) == 2 && getSouth(array,x,y) == 2)
+                {
+                    rotationAngle = Quaternion.Euler(0,0,-90);
+                }
+                
+                if (getWest(array,x,y) == 2 && getNorth(array,x,y) == 2)
+                {
+                    rotationAngle = Quaternion.Euler(0,0,180);
+                }
+
+                break;
+            case 2:
+                break;
+            case 3:
+                break;
+            case 4:
+                break;
+            case 5:
+                break;
+            case 6:
+                break;
+            case 7:
+                break;
         }
-        if (isInBounds(array, xPos - 1, yPos))
+        return rotationAngle;
+    }
+
+    private int getNorth(int[,] array, int x, int y)
+    {
+        if (isInBounds(array,x - 1,y))
         {
-            piece['E'] = tiles[levelMap[xPos - 1, yPos]];
-        }
-        if (isInBounds(array, xPos, yPos - 1))
-        {
-            piece['S'] = tiles[levelMap[xPos, yPos - 1]];
-        }
-        if (isInBounds(array, xPos + 1, yPos))
-        {
-            piece['W'] = tiles[levelMap[xPos + 1, yPos]];
+            return array[x - 1, y];
         }
 
-        return piece;
+        return 0;
+    }
+    
+    private int getEast(int[,] array, int x, int y)
+    {
+        if (isInBounds(array,x,y+1))
+        {
+            return array[x , y+1];
+        }
+
+        return 0;
+    }
+    
+    private int getSouth(int[,] array, int x, int y)
+    {
+        if (isInBounds(array,x + 1,y))
+        {
+            return array[x + 1, y];
+        }
+
+        return 0;
+    }
+    
+    private int getWest(int[,] array, int x, int y)
+    {
+        if (isInBounds(array,x,y - 1))
+        {
+            return array[x, y - 1];
+        }
+
+        return 0;
     }
     
     public static bool isInBounds(int[,] array, int x, int y)
