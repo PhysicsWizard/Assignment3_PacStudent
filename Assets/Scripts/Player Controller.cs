0d216c631ec27959bc2e058605cd477aa1392bc5
Arie Maljers <ariemaljers@gmail.com>
2023-10-06 17:48:29 +1100

    Hopeful Fix

---
+++ Assets/Scripts/Player Controller.cs
@@ -0,0 +1,56 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PlayerController : MonoBehaviour
+{
+    [SerializeField] private GameObject rabbit;
+    [SerializeField]private Tweener tweener;
+    [SerializeField] private Animator animator;
+    void Start()
+    {
+        StartCoroutine(moveMe());
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+    
+    IEnumerator moveMe()
+    {
+        Vector3 startPos = rabbit.transform.position;
+        Vector3 trueStart = startPos;
+        Vector3 endPos = new Vector3(startPos.x + 5f, startPos.y);
+        while (true)
+        {
+            tweener.AddTween(rabbit.transform, startPos, endPos,2f);
+            startPos = endPos;
+            endPos = new Vector3(endPos.x, endPos.y - 4.2f);
+            yield return new WaitForSeconds(2.1f);
+            
+            animator.SetFloat("Vertical", -1f);
+            tweener.AddTween(rabbit.transform, startPos, endPos,2f);
+            startPos = endPos;
+            endPos = new Vector3(endPos.x - 5f, endPos.y);
+            
+            yield return new WaitForSeconds(2.1f);
+            animator.SetFloat("Vertical", 0f);
+            animator.SetFloat("Horizontal", -1f);
+            tweener.AddTween(rabbit.transform, startPos, endPos,2f);
+            startPos = endPos;
+            endPos = new Vector3(endPos.x, endPos.y + 4.2f);
+            
+            yield return new WaitForSeconds(2.1f);
+            animator.SetFloat("Horizontal", 0f);
+            animator.SetFloat("Vertical", 1f);
+            tweener.AddTween(rabbit.transform, startPos, endPos,2f);
+            startPos = endPos;
+            endPos = new Vector3(endPos.x+5f, endPos.y);
+            yield return new WaitForSeconds(2.1f);
+            animator.SetFloat("Horizontal", 0f);
+            animator.SetFloat("Vertical", 0f);
+        }
+    }
+}
